326889ef0c8ee2f89e0210acfb62cbcc2d9caa5d
Charlotte Buchan <cebuc@talktalk.net>
2022-02-15 15:35:36 +0000

    added database and finished step 7

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,12 +4,9 @@ describe Bookmark do
   describe '.all' do
     it "returns all bookmarks" do 
       bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.google.com")
-    end
-
-    it "reads the bookmarks from the database"
-      bookmarks = Bookmark.all
-      expect(bookmarks).to respond_to
+      expect(bookmarks).to include("https://www.makersacademy.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
+      expect(bookmarks).to include("http://www.google.com/")
     end
   end
 end
